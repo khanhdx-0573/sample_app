4a3c9c66a551d038c36d9549d4b19e76c47c6ef0
khanhdx-0573 <dang.xuan.khanh@sun-asterisk.com>
2025-03-07 20:53:07 +0700

    Chapter 13: User microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-30.times do |i|
-  name = Faker::Name.name
-  email = "example-#{i+1}@gmail.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               activated: true, activated_at: Time.zone.now)
+users = User.order(:created_at).take(6)
+30.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
