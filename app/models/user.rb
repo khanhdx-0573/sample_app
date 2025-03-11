961a1b2aa62067c5c2c10ef930042da4dbbaddae
khanhdx-0573 <dang.xuan.khanh@sun-asterisk.com>
2025-03-11 10:44:06 +0700

    Chapter 14: Following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,14 @@ class User < ApplicationRecord
             length: {minimum: Settings.min_password_length}
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: Relationship.name,
+            foreign_key: :follower_id,
+            dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name,
+            foreign_key: :followed_id,
+            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   class << self
     def digest string_value
@@ -76,7 +84,20 @@ class User < ApplicationRecord
   end
 
   def feed
-    microposts.newest
+    Micropost.relate_post(following_ids << id).includes(:user,
+                                                        image_attachment: :blob)
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
   private
   def downcase_email
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-    get "static_pages/home"
-    get "static_pages/help"
+  get "static_pages/home"
+  get "static_pages/help"
 
-    get "/signup", to: "users#new"
-    post "/signup", to: "users#create"
-    resources :users
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
-    get "/login", to: "sessions#new"
-    post "/login", to: "sessions#create"
-    delete "/logout",to: "sessions#destroy"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout",to: "sessions#destroy"
 
-    resources :account_activations, only: :edit
-    resources :password_resets, only: %i(new create edit update)
+  resources :account_activations, only: :edit
+  resources :password_resets, only: %i(new create edit update)
 
-    resources :microposts, only: %i(create destroy)
+  resources :microposts, only: %i(create destroy)
+  resources :relationships, only: %i(create destroy)
 
-    root "static_pages#home"
+  root "static_pages#home"
 end
